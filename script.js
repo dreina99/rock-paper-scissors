5488c14ec0130dac2e9843bf15a1439a3efee911
dreina99 <devinreina99@gmail.com>
2023-06-30 16:28:51 -0700

    Build user choice function

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-choices = ["ROCK", "PAPER", "SCISSORS"]
+const choices = ["ROCK", "PAPER", "SCISSORS"]
 
 let getComputerChoice = () => {
     let randomIndex = Math.floor(Math.random() * choices.length)
     return choices[randomIndex]
 }
 
-computerChoice = getComputerChoice()
-console.log(computerChoice)
+let getUserChoice = () => {
+    let userChoice = prompt("Rock, paper, or scissors?")
+
+    while (true) {
+        if (choices.includes(userChoice.toUpperCase())) {
+            break
+        }
+        alert("Please enter rock, paper, or scissors")
+        userChoice = prompt("Rock, paper, or scissors?")
+    }
+    return userChoice.toUpperCase()
+}
+
+let computerChoice = getComputerChoice()
+let userChoice = getUserChoice()
+console.log(userChoice)
