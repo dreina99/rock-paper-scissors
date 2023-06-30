6da16b989eac87beadd532ce4470c99ea9e4a033
dreina99 <devinreina99@gmail.com>
2023-06-30 16:48:33 -0700

    Build basic game logic

--- script.js
+++ script.js
@@ -18,6 +18,77 @@ let getUserChoice = () => {
     return userChoice.toUpperCase()
 }
 
-let computerChoice = getComputerChoice()
-let userChoice = getUserChoice()
-console.log(userChoice)
+let printChoices = (round, computerChoice, userChoice) => {
+    console.log(`Round ${round}.`)
+    console.log(`Computer choice: ${computerChoice[0] + computerChoice.slice(1).toLowerCase()}`)
+    console.log(`User choice: ${userChoice[0] + userChoice.slice(1).toLowerCase()}`)
+    return
+}
+
+let game = () => {
+    let userScore = 0
+    let computerScore = 0
+    let round = 1
+    let computerChoice = ""
+    let userChoice = ""
+
+    while (userScore != 5 && computerScore != 5) {
+        computerChoice = getComputerChoice()
+        userChoice = getUserChoice()
+        printChoices(round, computerChoice, userChoice)
+
+        if(computerChoice === "ROCK") {
+            switch (userChoice) {
+                case "ROCK":
+                    console.log("Tie!")
+                    break;
+                case "SCISSORS":
+                    console.log("You lost this round!")
+                    computerScore += 1
+                    break;
+                case "PAPER":
+                    console.log("You won this round!")
+                    userScore += 1
+                    break;
+            }
+        } else if (computerChoice === "PAPER") {
+            switch (userChoice) {
+                case "ROCK":
+                    console.log("You lost this round!")
+                    computerScore += 1
+                    break;
+                case "SCISSORS":
+                    console.log("You won this round!")
+                    userScore += 1
+                    break;
+                case "PAPER":
+                    console.log("Tie!")
+                    break;
+            }
+        } else if (computerChoice == "SCISSORS") {
+            switch (userChoice) {
+                case "ROCK":
+                    console.log("You won this round!")
+                    userScore += 1
+                    break;
+                case "SCISSORS":
+                    console.log("Tie!")
+                    break;
+                case "PAPER":
+                    console.log("You lost this round!")
+                    computerScore += 1
+                    break;
+            }
+        }
+        round += 1
+    }
+
+    if (userScore == 5)
+        console.log("Woohoo! You won!")
+    else   
+        console.log("You lost. Better luck next time")
+    
+    return
+}
+
+game()
