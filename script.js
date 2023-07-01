30f612fc9f4cf7fe7a247ca8545b6104c52da9db
dreina99 <devinreina99@gmail.com>
2023-06-30 17:18:55 -0700

    Add round by round updates and simple UI

--- script.js
+++ script.js
@@ -25,64 +25,7 @@ let printChoices = (round, computerChoice, userChoice) => {
     return
 }
 
-let game = () => {
-    let userScore = 0
-    let computerScore = 0
-    let round = 1
-    let computerChoice = ""
-    let userChoice = ""
-
-    while (userScore != 5 && computerScore != 5) {
-        computerChoice = getComputerChoice()
-        userChoice = getUserChoice()
-        printChoices(round, computerChoice, userChoice)
-
-        if(computerChoice === "ROCK") {
-            switch (userChoice) {
-                case "ROCK":
-                    console.log("Tie!")
-                    break;
-                case "SCISSORS":
-                    console.log("You lost this round!")
-                    computerScore += 1
-                    break;
-                case "PAPER":
-                    console.log("You won this round!")
-                    userScore += 1
-                    break;
-            }
-        } else if (computerChoice === "PAPER") {
-            switch (userChoice) {
-                case "ROCK":
-                    console.log("You lost this round!")
-                    computerScore += 1
-                    break;
-                case "SCISSORS":
-                    console.log("You won this round!")
-                    userScore += 1
-                    break;
-                case "PAPER":
-                    console.log("Tie!")
-                    break;
-            }
-        } else if (computerChoice == "SCISSORS") {
-            switch (userChoice) {
-                case "ROCK":
-                    console.log("You won this round!")
-                    userScore += 1
-                    break;
-                case "SCISSORS":
-                    console.log("Tie!")
-                    break;
-                case "PAPER":
-                    console.log("You lost this round!")
-                    computerScore += 1
-                    break;
-            }
-        }
-        round += 1
-    }
-
+let displayResults = (userScore) => {
     if (userScore == 5)
         console.log("Woohoo! You won!")
     else   
@@ -91,4 +34,75 @@ let game = () => {
     return
 }
 
-game()
+let determineRound = (computerChoice, userChoice) => {
+    if(computerChoice === "ROCK") {
+        switch (userChoice) {
+            case "ROCK":
+                console.log("Tie!")
+                return "tie" 
+            case "SCISSORS":
+                console.log("You lost this round!")
+                return "computer"
+            case "PAPER":
+                console.log("You won this round!")
+                return "user"
+        }
+    } else if (computerChoice === "PAPER") {
+        switch (userChoice) {
+            case "ROCK":
+                console.log("You lost this round!")
+                return "computer"
+            case "SCISSORS":
+                console.log("You won this round!")
+                return "user"
+            case "PAPER":
+                console.log("Tie!")
+                return "tie"
+        }
+    } else if (computerChoice == "SCISSORS") {
+        switch (userChoice) {
+            case "ROCK":
+                console.log("You won this round!")
+                return "user"
+            case "SCISSORS":
+                console.log("Tie!")
+                return "tie"
+            case "PAPER":
+                console.log("You lost this round!")
+                return "computer"
+        }
+    }
+}
+
+let roundResults = (computerScore, userScore) => {
+    console.log(`Computer score: ${computerScore}`)
+    console.log(`User Score ${userScore}`)
+}
+
+let game = () => {
+    let userScore = 0
+    let computerScore = 0
+    let round = 1
+    let computerChoice = ""
+    let userChoice = ""
+    let winner = ""
+
+    while (userScore != 5 && computerScore != 5) {
+        computerChoice = getComputerChoice()
+        userChoice = getUserChoice()
+        printChoices(round, computerChoice, userChoice)
+
+        winner = determineRound(computerChoice, userChoice)
+        if (winner == "user")
+            userScore += 1
+        else if (winner == "computer")
+            computerScore += 1
+
+        round += 1
+        roundResults(computerScore, userScore)
+        console.log("\n")
+    }
+    
+    displayResults(userScore)
+    return
+}
