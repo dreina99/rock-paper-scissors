053320ddec2dd40703d4bd68b7e51e03b6c16bf6
dreina99 <devinreina99@gmail.com>
2023-07-03 15:23:31 -0700

    Finish game logic

--- script.js
+++ script.js
@@ -1,94 +1,147 @@
 const choices = ["ROCK", "PAPER", "SCISSORS"]
+let userScore = 0
+let computerScore = 0
+let round = 0
+
 const btns = document.querySelectorAll('button')
+const resultDiv = document.querySelector('.results')
+const dataDiv = document.querySelector('.round-data')
+const roundDiv = document.querySelector('.round-results')
+const gameDiv = document.querySelector('.game-results')
+
+btns.forEach(btn => btn.addEventListener
+    ('click', 
+    (e) => {
+        if (round === 0)
+            gameDiv.innerHTML = ""
+        round += 1
+        roundWinner = playRound(e.target.id.toUpperCase(), round)
+        if (roundWinner == "user")
+            userScore += 1
+        else if (roundWinner == "computer")
+            computerScore += 1
+        roundResults(computerScore, userScore)
+
+        if(userScore == 5 || computerScore == 5) {
+            displayResults(userScore)
+            resetGame()
+        }
+    })
+)
+
+let resetGame = () => {
+    userScore = 0
+    computerScore = 0
+    round = 0
+}
 
 let getComputerChoice = () => {
     let randomIndex = Math.floor(Math.random() * choices.length)
     return choices[randomIndex]
 }
 
-
 let printChoices = (round, computerChoice, userChoice) => {
-    console.log(`Round ${round}.`)
-    console.log(`Computer choice: ${computerChoice[0] + computerChoice.slice(1).toLowerCase()}`)
-    console.log(`User choice: ${userChoice[0] + userChoice.slice(1).toLowerCase()}`)
+    dataDiv.innerHTML = ""
+    let roundHeader = document.createElement('h3')
+    roundHeader.classList.add('round')
+
+    let para = document.createElement('p')
+    para.setAttribute('style', 'white-space:pre') // allow for \n line breaks
+    para.classList.add('choices')
+
+    roundHeader.textContent += `Round ${round}.\n`
+    dataDiv.appendChild(roundHeader)
+    
+    para.textContent += `Computer choice: ${computerChoice[0] + computerChoice.slice(1).toLowerCase()}\r\n`
+    para.textContent += `User choice: ${userChoice[0] + userChoice.slice(1).toLowerCase()}\n`
+    dataDiv.appendChild(para)
     return
 }
 
 let displayResults = (userScore) => {
+    gameDiv.internalHTML = ""
+    let para = document.createElement('p')
+    para.setAttribute('style', 'white-space:pre') // allow for \n line breaks
+
     if (userScore == 5)
-        console.log("Woohoo! You won!")
+        para.textContent += "Woohoo! You won!\n"
     else   
-        console.log("You lost. Better luck next time")
+        para.textContent += "You lost. Better luck next time.\n"
     
+    para.textContent += "Click any option to play again."
+    gameDiv.appendChild(para)
     return
 }
 
 let determineRound = (computerChoice, userChoice) => {
+    roundDiv.innerHTML = ""
+    let para = document.createElement('p')
+    para.setAttribute('style', 'white-space:pre')
     if(computerChoice === "ROCK") {
         switch (userChoice) {
             case "ROCK":
-                console.log("Tie!")
+                para.textContent += "Tie!"
+                roundDiv.appendChild(para)
                 return "tie" 
             case "SCISSORS":
-                console.log("You lost this round!")
+                para.textContent = "You lost this round!"
+                roundDiv.appendChild(para)
                 return "computer"
             case "PAPER":
-                console.log("You won this round!")
+                para.textContent = "You won this round!"
+                roundDiv.appendChild(para)
                 return "user"
         }
     } else if (computerChoice === "PAPER") {
         switch (userChoice) {
             case "ROCK":
-                console.log("You lost this round!")
+                para.textContent += "You lost this round!"
+                roundDiv.appendChild(para)
                 return "computer"
             case "SCISSORS":
-                console.log("You won this round!")
+                para.textContent = "You won this round!"
+                roundDiv.appendChild(para)
                 return "user"
             case "PAPER":
-                console.log("Tie!")
+                para.textContent = "Tie!"
+                roundDiv.appendChild(para)
                 return "tie"
         }
     } else if (computerChoice == "SCISSORS") {
         switch (userChoice) {
             case "ROCK":
-                console.log("You won this round!")
+                para.textContent = "You won this round!"
+                roundDiv.appendChild(para)
                 return "user"
             case "SCISSORS":
-                console.log("Tie!")
+                para.textContent = "Tie!"
+                roundDiv.appendChild(para)
                 return "tie"
             case "PAPER":
-                console.log("You lost this round!")
+                para.textContent = "You lost this round!"
+                roundDiv.appendChild(para)
                 return "computer"
         }
     }
 }
 
 let roundResults = (computerScore, userScore) => {
-    console.log(`Computer score: ${computerScore}`)
-    console.log(`User Score ${userScore}`)
+    let para = document.createElement('p')
+    para.setAttribute('style', 'white-space:pre')
+
+    para.textContent += `Computer score: ${computerScore}\n`
+    para.textContent += `User Score ${userScore}\n` 
+
+    roundDiv.appendChild(para)
 }
 
-btns.forEach(btn => btn.addEventListener('click', (e) => playRound(e.target.id.toUpperCase())))
-
-let playRound = (userChoice) => {
-    let userScore = 0
-    let computerScore = 0
-    let round = 1
+let playRound = (userChoice, roundNumber) => {
     let computerChoice = ""
     let winner = ""
-
+    
     computerChoice = getComputerChoice()
-    printChoices(round, computerChoice, userChoice)
+    printChoices(roundNumber, computerChoice, userChoice)
 
     winner = determineRound(computerChoice, userChoice)
-    if (winner == "user")
-        userScore += 1
-    else if (winner == "computer")
-        computerScore += 1
-
-    round += 1
-    roundResults(computerScore, userScore)
-    console.log("\n")
-
-    return
+    return winner
 }
